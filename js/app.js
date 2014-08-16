453466ca9b743cb90fd35005282d73e5240156f7
eferrao <elizabethferrao@Elizabeths-MacBook-Air.local>
2014-08-16 15:20:04 -0400

    added basic file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-// Your JS code goes here.
+// PART 1: STICKY HEADER
+// Write an event listener which detects when the window is scrolled.
+// When the scroll position of the page is at or beyond the top of the header,
+// add the class 'sticky' to the header.
